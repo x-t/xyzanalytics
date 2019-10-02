f9a476c439e77c504b63c15e7c97b3572da2c294
Doggoli <55459757+doggoley@users.noreply.github.com>
2019-10-02 09:02:06 -0700

    lol
    lol

--- include.php
+++ include.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * xyzanalytics - A small analytics script
- * Written in 2018 by cmp cmp@airmail.cc
+ * Written with love in 2018 by cmp cmp@airmail.cc
  * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
  * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
